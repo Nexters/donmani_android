9381a9056edab9089e6e2c557e529a59abef4ee4
gowoon-choi <gowoonvv@gmail.com>
2025-03-27 13:34:50 +0900

    닉네임 전부 지웠을 때 토스트 에러 수정

--- core/domain/src/main/java/com/gowoon/domain/util/NicknameUtil.kt
+++ core/domain/src/main/java/com/gowoon/domain/util/NicknameUtil.kt
@@ -3,8 +3,12 @@ package com.gowoon.domain.util
 object NicknameUtil {
     const val NICKNAMEMAX_LENGTH = 12
     const val DEFAULT_SUFFIX = "님의 별통이"
-    fun isValid(input: String): Boolean {
-        val regex = "^[a-zA-Z0-9ㄱ-ㅎ가-힣ㅏ-ㅣ\\s]{1,12}$".toRegex()
+    fun isValidLength(input: String): Boolean {
+        return input.length in 2..12
+    }
+
+    fun isValidCharacter(input: String): Boolean {
+        val regex = "^[a-zA-Z0-9ㄱ-ㅎ가-힣ㅏ-ㅣ\\s]*$".toRegex()
         return regex.matches(input)
     }
 }
