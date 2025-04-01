970fff13c6a4077173f2e7c212468043f9cbca38
gowoon-choi <gowoonvv@gmail.com>
2025-04-01 11:16:28 +0900

    알림 권한 요청 추가

--- app/src/main/java/com/gowoon/donmani_android/MainActivity.kt
+++ app/src/main/java/com/gowoon/donmani_android/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.gowoon.donmani_android
 
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import com.gowoon.designsystem.theme.DonmaniTheme
@@ -11,13 +15,23 @@ import com.gowoon.donmani_android.navigation.DonmaniNavHost
 import com.gowoon.ui.BGMode
 import com.gowoon.ui.GradientBackground
 import dagger.hilt.android.AndroidEntryPoint
+import io.github.aakira.napier.Napier
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val notificationPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        if (isGranted) {
+            // TODO 
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        checkNotificationPermission()
         setContent {
             DonmaniTheme {
                 val navController = rememberNavController()
@@ -25,6 +39,20 @@ class MainActivity : ComponentActivity() {
                 GradientBackground(if (appState.isBeforeHome) BGMode.SPECIAL else BGMode.DEFAULT) {
                     DonmaniNavHost(navController = navController)
                 }
+            }
+        }
+    }
+
+    private fun checkNotificationPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.POST_NOTIFICATIONS
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                Napier.d("gowoon 권한이 이미 허용됨")
+            } else {
+                notificationPermissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS)
             }
         }
     }
